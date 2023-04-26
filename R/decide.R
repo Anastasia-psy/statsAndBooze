7430ab9ea207c7efec6e56754aef87c87122c1d0
Anastasia-psy <a.galkina@campus.unimib.it>
2023-04-26 18:38:50 +0200

    fix docs + enhance parse with the possibility of specifying intervals + docs

--- R/decide.R
+++ R/decide.R
@@ -1,12 +1,21 @@
-#' Title Decide the best day gor a happy hour.
+#' Intersect a list of dates
 #'
-#' @param x A list of date vectors
+#' @param x A list of elements of class Date that describe when someone is
+#' available for a beer. Commonly an output of the parse_dates() function.
 #'
-#' @return The set of common dates
+#' @return A list of Dates that are common for all elements of the list
+#' (the Dates when everyone is available for a beer).
 #' @export
 #'
-#' @examples 1+1
+#' @examples
+#' list_dates <- list(
+#'   a = c("2023-04-01", "2023-04-03 / 2023-04-05"),
+#'   b = c("2023-04-02", "2023-04-03"),
+#'   c = "2023-04-02 / 2023-04-03"
+#' )
+#'
+#' parsed_dates <- parse_dates(list_dates)
+#' decide_happy_hour(parsed_dates)
 decide_happy_hour <- function(x) {
   lubridate::as_date(Reduce(lubridate::intersect , x))
 }
-2+2
--- R/parse.R
+++ R/parse.R
@@ -1,11 +1,29 @@
-#' Parse string dates into dates
+#' Parse list of strings into dates
 #'
-#' @param x a list of character dates
+#' @details Please notice that each date must be specified using the
+#' YYYY-MM-DD format. A time interval should be specified using the following format:
+#' "YYYY-MM-DD / YYYY-MM-DD", where the first date is the start of the interval,
+#' and the second date is the end of the interval.
 #'
-#' @return a list of Date vectors
+#' @param x A list of strings that describe when someone is available for a beer
+#'
+#' @return A list of Dates when someone is available for a beer. The strings are
+#' converted into objects of class Date. The intervals are represented by a sequence of
+#' Dates within the interval.
 #' @export
 #'
-#' @examples 1 + 1
-parse_dates <- function(x){
-  lapply(x, lubridate::as_date)
+#' @examples
+#' list_dates <- list(
+#'   a = c("2023-04-01", "2023-04-03 / 2023-04-05"),
+#'   b = c("2023-04-02", "2023-04-03"),
+#'   c = "2023-04-02 / 2023-04-03"
+#' )
+#' (parse_dates(list_dates))
+parse_dates<-function(x) {
+  lapply(x, function(x) {
+    lubridate::as_date(unlist(mapply(function(x,y) seq(x, y, by=1),
+                                     lubridate::ymd(lubridate::int_start(lubridate::interval(x))),
+                                     lubridate::ymd(lubridate::int_end(lubridate::interval(x))))), origin="1970-01-01")
+  }
+  )
 }
